9d04f3542f3e39b2bdc0b562c79b9012484a2e31
Hernan Funes <funes.hernan@hotmail.es>
2026-08-15 10:46:36 -0300

    primer push

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace inmobiliariaFUNES.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
