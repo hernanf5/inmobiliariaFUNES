29bc68eb56883e1ede0ed7064796017a88deec54
Hernan Funes <funes.hernan@hotmail.es>
2026-09-15 14:35:25 -0300

    fix:Pago en program.cs

--- Program.cs
+++ Program.cs
@@ -22,6 +22,7 @@ builder.Services.AddScoped<IRepositorioInmueble, RepositorioInmueble>();
 builder.Services.AddScoped<IRepositorioReserva, RepositorioReserva>();
 builder.Services.AddScoped<ServicioHash>();
 builder.Services.AddScoped<IRepositorioUsuario, RepositorioUsuario>();
+builder.Services.AddScoped<IRepositorioPago, RepositorioPago>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
